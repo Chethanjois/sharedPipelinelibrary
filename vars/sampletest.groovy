ea4648e08b0e7a0320c3c181adef179531e04a84
Chethanjois <35957098+Chethanjois@users.noreply.github.com>
2021-11-11 20:06:07 +0530

    Update sampletest.groovy

---
+++ vars/sampletest.groovy
@@ -0,0 +1,18 @@
+def call (body) {
+
+def config = [:]
+body.resolveStrategy = Clousre.DELEGATE_FIRST
+body.delegate = config
+body()
+
+pipeline {
+  agent any
+  stages {
+    stage ('Build')
+    {
+      sh 'echo "Helloworld"'
+    }
+  }
+}
+
+}
